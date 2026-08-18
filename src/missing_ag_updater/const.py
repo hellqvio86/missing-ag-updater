6435f22599c6ac89e4fcca2f1335e47d080a0f8f
Olof Hellqvist <olof.hellqvist@gmail.com>
2026-08-18 21:17:54 +0200

    chore: bump version to 0.3.1

--- src/missing_ag_updater/const.py
+++ src/missing_ag_updater/const.py
@@ -25,7 +25,7 @@ def _is_ubuntu_style_distro() -> bool:
     return False
 
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 # Truthy string values for environment variables and config parsing
 TRUTHY_VALUES: tuple[str, ...] = ("1", "true", "yes", "on")
